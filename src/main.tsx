4e5578a5a32c94e1f2f539b1c75e2a713b9c71ca
杨文华 <1289978696@qq.com>
2025-12-08 22:42:53 +0800

    feat: 接入sentry 监控

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './routes'
+import * as Sentry from "@sentry/react";
 
-createRoot(document.getElementById('root')!).render(
+Sentry.init({
+  dsn: "https://115312e36450246c6f2cab50c5432314@o4510499341008896.ingest.us.sentry.io/4510499342385152",
+  sendDefaultPii: true
+});
+
+const container = document.getElementById('root');
+
+createRoot(container!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,22 @@
+import { Button } from "antd"
 
 const Home = () => {
 
-    console.log("Home")
+    console.log("Home");
+
+    const onError = () => {
+        throw new Error("测试错误");
+    }
 
     return (
         <>
             <h1>Home</h1>
+            <Button onClick={onError}>
+                错误上报测试
+            </Button>
+            <Button type="primary" danger onClick={() => {
+                
+            }}>报错啦</Button>
         </>
     )
 }
